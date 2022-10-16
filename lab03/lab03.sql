89f763947166de9f27ee917cb1def6c6784a3236
poliorang <poliorang05@mail.ru>
2022-10-16 20:19:37 +0300

    Final lab03

--- lab03/lab03.sql
+++ lab03/lab03.sql
@@ -1,7 +1,7 @@
-drop function authorCountry(author varchar(100))
+--lab03
 
---------Скалярная функция--------
-
+--1
+--Скалярная функция
 --Количество тарифов, стоимость которых больше CurrentPriceTariff
 create or replace function public.priceTariff (CurrentPriceTariff INT)
 returns int as
@@ -12,6 +12,256 @@ language SQL
 
 select public.priceTariff(1000)
 
+
+--2
+--Подставляемная табличная функция
+--Название, поддерживаемые устройста и страна сервиса, поддерживаемые устройства которого соответствуют заданному
+create or replace function supportedDevicesFunc(Divices varchar(10) = 'Phone')
+returns table
+(
+	NameService varchar(100),
+	SupportedDeviceService varchar(10),
+	NameCountry varchar(100)
+)
+as 
+	'select NameService, SupportedDeviceService, NameCountry
+	from Songs.Services s join Country.Countries c on s.CountryId = c.CountryId
+	where SupportedDeviceService like Divices'
+language SQL
+
+select * from supportedDevicesFunc()
+select * from supportedDevicesFunc('PC')
+
+
+--3
+--Многооператорная табличная функция
+--Песни, продолжительность которых меньше, чем заданная, и дата создания более ранняя, чем заданная
+create or replace function durationSongForDate(duration float, dateRelease date)
+returns table
+(
+	NameSong varchar(100),
+	AuthorSong varchar(100),
+	ReleaseSong date,
+	DurationSong float
+) as 
+'begin
+	return query
+	select s.NameSong, s.AuthorSong, s.ReleaseSong, s.DurationSong
+	from Songs.Songs s
+	where s.DurationSong < duration;
+
+	return query
+	select s.NameSong, s.AuthorSong, s.ReleaseSong, s.DurationSong
+	from Songs.Songs s
+	where s.ReleaseSong < dateRelease;
+end;' 
+language plpgsql
+
+select * from durationSongForDate(3.0, '01-01-2000');
+
+
+--4
+--Рекурсивная функция
+--Числа Фиббоначчи
+create or replace function fib(first INT, second INT,max INT)
+returns table (fibonacci INT)
+as 
+'begin
+	return query
+	select  first;
+    if second <= max then
+        return query
+        select *
+        from fib(second, first + second, max);
+    end if;
+end' 
+language plpgsql
+
+select * from fib(1, 1, 13)
+
+
+--5
+--Хранимая процедура без параметров или с параметрами
+--Увеличить стоимость тарифа в данное количество раз, если количество пользователей больше определенного
+create or replace procedure changePrice
+(
+	coeff float,
+	CountOfUser int	
+)
+as 
+'begin
+	update Songs.Tariff
+    set PriceTariff = (PriceTariff * coeff)
+    where CountOfUsersTariff > CountOfUser;
+end;' 
+language plpgsql
+   
+call changePrice(1.5, 90000)
+select * from Songs.Tariff where CountOfUsersTariff > 90000 order by CountOfUsersTariff
+
+
+--6
+--Рекурсивная хранимая процедура или хранимая процедура с рекурсивным ОТВ
+--Числа Фибоначчи
+create or replace procedure fib_index
+(
+	res inout int,
+	index_ int,
+	start_ int default 1, 
+	end_ int default 1
+)
+as 
+'BEGIN
+	IF index_ > 0 THEN
+		res = start_ + end_;
+		CALL fib_index(res, index_ - 1, end_, start_ + end_);
+	END IF;
+END;' 
+language plpgsql
+
+call fib_index(1, 5)
+
+
+--7
+--Хранимая процедура с курсором
+--Названия тарифов, которые начинаются на заданную подстроку
+create or replace procedure TariffTitle
+(
+	inputText varchar(100)
+)
+as 
+'declare
+	title varchar(100);
+    myCursor cursor 
+	for
+        select TitleTariff
+		from Songs.Tariff t
+		where TitleTariff like inputText;
+begin
+    open myCursor;
+    loop
+        fetch myCursor
+        into title;
+        exit when not found;
+        raise notice ''Tariff Title =  %'', title;
+    end loop;
+    close myCursor;
+end;'
+language plpgsql;
+
+call TariffTitle('A%');
+
+
+--8
+--Хранимая процедура доступа к метаданным
+--Информация о типах полей заданной таблицы
+create or replace procedure metaData
+(
+	tablename varchar(100)
+)
+as 
+'declare
+	buf record;
+    myCursor cursor 
+	for
+        select column_name, data_type
+		from information_schema.columns
+        where table_name = tablename;
+begin
+    open myCursor;
+    loop
+		fetch myCursor
+        into buf;
+		exit WHEN NOT FOUND;
+        raise notice ''column = %; data type = %'', buf.column_name, buf.data_type;
+    end loop;
+	close myCursor;
+end;' 
+language plpgsql;
+
+call metaData('services');
+
+
+--9
+--Триггер AFTER
+
+--Создаем временную таблицы
+select * 
+into countryTmp
+from Country.Countries
+
+--Создаем функцию для триггера
+create or replace function updateTrigger()
+returns trigger
+as 
+'begin
+	raise notice ''New =  %'', new;
+    raise notice ''Old =  %'', old; 
+	update countryTmp
+	set countryid = new.countryid
+	where countryTmp.countryid = old.countryid;
+	
+	return new;
+end;' 
+language plpgsql;
+
+--Создаем триггер
+create trigger update_my
+after update on countryTmp 
+for each row 
+execute procedure updateTrigger();
+
+--Изменяем поле (при изменении сработает триггер)
+update countryTmp 
+set namecountry = 'bbbbb'
+where countryid = 1
+
+--Проверяем, что название страны с ид 1 не изменилось
+select * from countryTmp order by countryid 
+
+
+--10
+--Триггер INSTEAD OF
+
+--Делаем временную вью
+create view songsTMP as 
+select * 
+from songs.songs
+select * from songsTMP
+
+--drop view songsTMP
+
+--Создаем функцию для триггера
+create or replace function deleteForCountry()
+returns trigger 
+as 
+'begin
+    raise notice ''New =  %'', new;
+    update songsTMP
+    set NameSong = ''none'' 
+    where songsTMP.NameSong = old.NameSong;
+    return new;
+end;' 
+language plpgsql;
+
+--drop trigger deleteForCountryTrigger on songsTMP
+
+--Создаем триггер
+create trigger deleteForCountryTrigger
+instead of delete on songsTMP
+	for each row
+	execute procedure deleteForCountry()
+
+--Пытаемся удалить кортеж по условию
+delete from songsTMP 
+where songsTMP.countryid = 2
+
+--Проверяем, что имена просто заменили на none, но не удалились
+select * from songsTMP order by countryid
+
+
+
+--Еще запросики
 --Наименьшая длина песни, из множества песен, которые были написаны в заданный промежуток времени
 create or replace function public.dateSong (minDate date, maxDate date)
 returns float as
@@ -32,26 +282,6 @@ language SQL
 
 select public.avgDurationForCountry(1)
 
-
-
---------Подставляемная табличная функция--------
-
---Название, поддерживаемые устройста и страна сервиса, поддерживаемые устройства которого соответствуют заданному
-create or replace function supportedDevicesFunc(Divices varchar(10) = 'Phone')
-returns table
-(
-	NameService varchar(100),
-	SupportedDeviceService varchar(10),
-	NameCountry varchar(100)
-)
-as 
-	'select NameService, SupportedDeviceService, NameCountry
-	from Songs.Services s join Country.Countries c on s.CountryId = c.CountryId
-	where SupportedDeviceService like Divices'
-language SQL
-
-select * from supportedDevicesFunc()
-select * from supportedDevicesFunc('PC')
 
 --Имена, гендер и дата рождения авторов, которые родились в указанный промежуток
 create or replace function BirthDateAuthorFunc(minDate date, maxDate date)
@@ -84,34 +314,6 @@ language SQL
 	
 select * from NameCountryFunc('Spain')
 
-
-
---------Многооператорная табличная функция--------
-
---Песни, продолжительность которых меньше, чем заданная, и дата создания более ранняя, чем заданная
-create or replace function durationSongForDate(duration float, dateRelease date)
-returns table
-(
-	NameSong varchar(100),
-	AuthorSong varchar(100),
-	ReleaseSong date,
-	DurationSong float
-) as 
-'begin
-	return query
-	select s.NameSong, s.AuthorSong, s.ReleaseSong, s.DurationSong
-	from Songs.Songs s
-	where s.DurationSong < duration;
-
-	return query
-	select s.NameSong, s.AuthorSong, s.ReleaseSong, s.DurationSong
-	from Songs.Songs s
-	where s.ReleaseSong < dateRelease;
-end;' 
-language plpgsql
-
-select * from durationSongForDate(3.0, '01-01-2000');
-
 --Авторы, чье имя начинается на заданную подстроку, и их страна
 create or replace function authorCountry(author varchar(100))
 returns table
@@ -128,72 +330,3 @@ end;'
 language plpgsql
 
 select * from authorCountry('J%')
-
-
-
---------Рекурсивная функция--------
-
---Числа Фиббоначчи
-create or replace function fib(first INT, second INT,max INT)
-returns table (fibonacci INT)
-as 
-'begin
-	return query
-	select  first;
-    if second <= max then
-        return query
-        select *
-        from fib(second, first + second, max);
-    end if;
-end' 
-language plpgsql
-
-select * from fib(1, 1, 13)
-
-
-
---Хранимая процедура без параметров или с параметрами
-
---Увеличить стоимость тарифа в данное количество раз, если количество пользователей больше определенного
-create or replace procedure changePrice
-(
-	coeff float,
-	CountOfUser int	
-)
-as 
-'begin
-	update Songs.Tariff
-    set PriceTariff = (PriceTariff * coeff)
-    where CountOfUsersTariff > CountOfUser;
-end;' 
-language plpgsql
-   
-call changePrice(1.5, 90000)
-select * from Songs.Tariff where CountOfUsersTariff > 90000 order by CountOfUsersTariff
-
-
-
---Рекурсивная хранимая процедура или хранимая процедура с рекурсивным ОТВ
-create or replace procedure fib_index
-(
-	res inout int,
-	index_ int,
-	start_ int default 1, 
-	end_ int default 1
-)
-as 
-'BEGIN
-	IF index_ > 0 THEN
-		res = start_ + end_;
-		CALL fib_index(res, index_ - 1, end_, start_ + end_);
-	END IF;
-END;' 
-language plpgsql
-
-call fib_index(1, 5)
-
-
-
-
-
-
